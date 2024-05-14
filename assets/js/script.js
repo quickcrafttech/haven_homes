e2e17b08955550d5458dcb0799d34cdabf72706a
naseralam <40887297+naserAlam@users.noreply.github.com>
2024-05-14 17:06:07 +0600

    header-section

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Navbar toggle in moblie
+ */
+
+const $navbar = document.querySelector("[data-navbar]");
+const $navToggler = document.querySelector("[data-nav-toggler]");
+
+$navToggler.addEventListener("click", () => $navbar.classList.toggle("active"));
+
+/**
+ * Header scroll state
+ */
+
+const $header = document.querySelector("[data-header]");
+window.addEventListener("scroll", e => {
+    $header.classList[window.scrollY > 50 ? "add" : "remove"]("active");
+});
